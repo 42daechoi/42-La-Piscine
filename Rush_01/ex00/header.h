bfe9fb236b2d851c1cd69153090fcaa14bf6d1ba
Daeyoung Choi <daechoi@c1r5s3.42seoul.kr>
2021-09-18 21:39:32 +0900

    commit

--- Rush_01/ex00/header.h
+++ Rush_01/ex00/header.h
@@ -6,7 +6,7 @@
 /*   By: daechoi <daechoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 18:49:30 by daechoi           #+#    #+#             */
-/*   Updated: 2021/09/18 21:25:37 by daechoi          ###   ########.fr       */
+/*   Updated: 2021/09/18 21:39:00 by daechoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef HEADER_H
@@ -15,20 +15,6 @@
 extern int	*g_input;
 
 extern int	**g_arr;
-
-void	ft_putchar(char c);
-
-void	ft_print(int n);
-
-int		ft_up_is_correct(int x, int y, int n);
-
-int		ft_down_is_correct(int x, int y, int n);
-
-int		ft_left_is_correct(int x, int y, int n);
-
-int		ft_right_is_correct(int x, int y, int n);
-
-int		ft_is_exist(int x, int y, int n);
 
 int		ft_is_correct(int x, int y, int n);
 
